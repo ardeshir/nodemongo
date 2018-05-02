395c5336a2cd488f1d109b6b2ceb1e42bbd3ba15
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-05-02 21:45:29 +0000

    adding routs

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const url  = require('url');
 
 const host = '0.0.0.0';
 const port    = '9000';
@@ -18,11 +19,20 @@ const arrStations = [
     
     
 const server  = http.createServer((req, res) => {
-    
+      
+      var pathname = url.parse(req.url, true).pathname;
+      
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(arrStations));
+      
+      if(pathname.indexOf('first') != -1) {
+          
+      res.end(JSON.stringify(arrStations[0]));
     
+      } else {
+       res.end(JSON.stringify(arrStations));         
+                
+      }
 });
 
 server.listen(port, host,  () => {
